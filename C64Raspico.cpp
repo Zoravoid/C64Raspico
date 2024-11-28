4759b27bbc9b74dba5d58df16ef9863c2ae3e451
Zoravoid <144455312+Zoravoid@users.noreply.github.com>
2024-11-28 22:02:43 +0100

    Update 1

--- C64Raspico.cpp
+++ C64Raspico.cpp
@@ -3,6 +3,8 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "display.h"
+#include "button.h"
 
 #define LED_PIN 7
 #define BUTTON_PIN 10
@@ -15,42 +17,82 @@ char pass[] = "Password";
 
 int main() 
 {
-     studio_init_all();
-     
+    studio_init_all(); 
+    display oled;
+    Button input_a(10);
 }
-
-
-class display
-{
-
-};
 
 class wifi
 {
 
 };
 
+class alarm
+{
+    void Alarm()
+    {
+        while(true)
+        {
+        gpio_put(LED_PIN, 1); 
+        gpio_put(BUZZER_PIN, 1);
+        sleep(50);
+        gpio_put(LED_PIN, 0);
+        gpio_put(BUZZER_PIN, 0);
+        sleep(50);
+        }  
+    }
+};
+
 void Clean()
 {
-
+    gpio_init(OLED_PIN);
+    gpio_set_dir(OLED_PIN, GPIO_OUT);
+    while(true)
+    {
+        oled._pfrintf("Ready for Cleanup!");
+        sleep_ms(100);
+        oled.clear(false);
+        sleep_ms(100);
+    }
 }
 
 void Sleep()
 {
-
+    alarm;
+    gpio_init(OLED_PIN);
+    gpio_set_dir(OLED_PIN, GPIO_OUT);
+    for(int i = 0; i < 3; i++)
+    {
+        oled._pfrintf("Good Night!");
+        sleep_ms(100);
+        oled.clear(false);
+        sleep_ms(100);
+    }
 }
 
 void Overtime()
 {
-
+    gpio_init(OLED_PIN);
+    gpio_set_dir(OLED_PIN, GPIO_OUT);
+    for(int i = 0; i < 3; i++)
+    {
+        oled._pfrintf("Overtime Enabled!");
+        sleep_ms(100);
+        oled.clear(false);
+        sleep_ms(100);
+    }
 }
 
 void Changestate()
 {
+    alarm;
     gpio_init(OLED_PIN);
-     gpio_set_dir(OLED_PIN, GPIO_OUT);
-     while (true) {
-         gpio_put(BUZZER_PIN, 1); sleep_ms(250);
-         
+    gpio_set_dir(OLED_PIN, GPIO_OUT);
+    while(true)
+    {
+        oled._pfrintf("Change Height!");
+        sleep_ms(100);
+        oled.clear(false);
+        sleep_ms(100);
     }
 }
