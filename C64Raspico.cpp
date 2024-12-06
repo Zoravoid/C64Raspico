1f9562a51662f39dbdeb6aba683ff3476eac6163
Zoravoid <144455312+Zoravoid@users.noreply.github.com>
2024-12-06 15:48:50 +0100

    u11

--- C64Raspico.cpp
+++ C64Raspico.cpp
@@ -6,144 +6,82 @@
 #include "button.h"
 #include "lwipopts.h"
 
-
 #define LED_PIN 7
 #define BUTTON_PIN 10
 #define OLED_PIN 4
 #define BUZZER_PIN 20
 
-//Edit these parameters to fit your local network
-//still need to fix CYW43_ARCH for wifi to work
-char ssid[] = "Network Name";
-char pass[] = "Password";
-char request = (char *)p->payload; //error
+char ssid[] ="Network Name";
+char pass[] ="Password";
 
-int main() 
+Button button(BUTTON_PIN);
+display oled;
+
+
+int main()
 {
-    studio_init_all(); //error
-    display oled;
-    Button button(BUTTON_PIN, debounce_time, true, true, true); //error
-    const unit32_t debounce_time = 50; //error
-
+    stdio_init_all();
+    
     oled.clear();
-    oled._printf("Initializing WiFi...\n"); //error
+    oled._printf(1, true, "Initialize WiFi...\n");
     if(cyw43_arch_init())
     {
         oled.render();
-        oled._printf("Failed to initialize CYW43!\n"); //error
-        return -1;
-    }
-    
-    oled.render();
-    oled._printf("Connecting to Network:\n", ssid); //error
-    if(cyw43_arch_wifi_connect_blocking(ssid, pass, CYW43_AUTH_WPA2_MIXED_PSK))
-    {
-        oled.render();
-        oled._printf("Failed to connect to Wifi!\n"); //error
+        oled._printf(2, true, "Failed to initialize cyw43!\n");
         return -1;
     }
 
     oled.render();
-    oled._printf("Connected to WiFi!\n"); //error
+    oled._printf(3, true, "Connecting to Network:\n", ssid);
+    if(cyw43_arch_wifi_connect_blocking(ssid, pass, CYW43_AUTH_WPA2_MIXED_PSK))
+    {
+        oled.render();
+        oled._printf(4, true, "Failed to connect to Wifi!\n");
+        return -1;
+    }
+
+    oled.render();
+    oled._printf(5, true, "Connected to WiFi!");
+    sleep_ms(1000);
+    oled.clear(false);
 
     while(true)
     {
         cyw43_arch_poll();
         if(button.is_pressed())
         {
-            Overtime();
-            while(button.is_pressed())
-            {
-                sleep_ms(10);
-            }
+
         }
-
-        //Add corect http request
-        switch(request)
-        {
-            case "Clean request": //error
-                Clean();
-                break;
-            case "Sleep request": //error
-                Sleep();
-                break;
-            case "Overtime request": //error
-                Overtime();
-                break;
-            case "Changestate request": //error
-                Changestate();
-                break;
-        }
-    }
-
-    cyw43_arch_deinit();
-    return 0;
-}
-
-class wifi
-{
-    //if pico needs more http
-};
-
-
-void Alarm()
-{
-    gpio_init(LED_PIN, BUZZER_PIN); //error
-    gpio_set_dir(LED_PIN, BUZZER_PIN, GPIO_OUT) //error
-    while(true) //error
-    {
-    gpio_put(LED_PIN, 1); 
-    gpio_put(BUZZER_PIN, 1);
-    sleep(50);
-    gpio_put(LED_PIN, 0);
-    gpio_put(BUZZER_PIN, 0);
-    sleep(50);
-    }  
-}
-
-
-void Clean()
-{
-    for(int i = 0; i < 10; i++)
-    {
-        oled._pfrintf("Ready for Cleanup!"); //error
-        sleep_ms(100);
-        oled.clear(false);
-        sleep_ms(100);
-    }
-}
-
-void Sleep()
-{
-    Alarm();
-    for(int i = 0; i < 3; i++)
-    {
-        oled._pfrintf("Good Night!"); //error
-        sleep_ms(100);
-        oled.clear(false);
-        sleep_ms(100);
     }
 }
 
 void Overtime()
 {
-    for(int i = 0; i < 3; i++)
+    for(int i=0; i<3; i++)
     {
-        oled._pfrintf("Overtime Enabled!"); //error
+        oled._printf(1, true, "Overtime Enabled!");
         sleep_ms(100);
         oled.clear(false);
         sleep_ms(100);
     }
 }
 
+void Alarm()
+{
+    
+}
+
+void Clean()
+{
+
+}
+
+void Sleep()
+{
+
+}
+
 void Changestate()
 {
-    Alarm();
-    for(int i = 0; i < 10; i++)
-    {
-        oled._pfrintf("Change Height!"); //error
-        sleep_ms(100);
-        oled.clear(false);
-        sleep_ms(100);
-    }
+
 }
